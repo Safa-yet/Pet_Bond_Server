a6b4584186f97852a3a71a32a819dc27d8e8ecaa
Safayet <mdsafayet315@gmail.com>
2026-05-20 01:20:54 +0600

    Added Post animal

--- index.js
+++ index.js
@@ -1,10 +1,72 @@
-const express = require('express');
-const app = express();
-const port = 5000;
+const express = require('express')
+const app = express()
+const port = process.env.SERVER_PORT || 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+const uri = process.env.MONGODB_URI
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+// console.log(client);
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+      const db= client.db("petbond");
+    const dbCollection =db.collection("animal");
+    const adoptCollection = db.collection("adopt");
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    
+app.get('/',async (req, res) => {
+  res.send('Hello World!')
+  console.log(uri);
+})
+    app.get('/animal',  async(req,res)=>{
+      const cursor =await dbCollection.find();
+      const result = await cursor.toArray();
+      console.log(result);
+      res.send(result);
+    })
+
+
+    app.post('/animal', async(req,res)=>{
+      const newPet = req.body;
+      const result = await dbCollection.insertOne(newPet);
+      res.send(result);
+    })  
+
+
+    
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
